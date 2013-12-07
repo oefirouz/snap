e1bc9695f5a86cd50c0644685b9392a082a988c3
Omede <omedefirouz@gmail.com>
2013-12-06 16:44:39 -0800

    SPEEEEEEEEEEEEEEEED

--- examples/flow/pushrelabel.cpp
+++ examples/flow/pushrelabel.cpp
@@ -5,7 +5,7 @@
 
 #define INF (1 << 30)
 #define MIN(A,B) ((A < B) ? (A) : (B))
-#define GLOBAL_UPDATE_FREQ 1.0
+#define GLOBAL_UPDATE_FREQ 0.5
 
 void net_from_dimacs(PNEANet G, char *filename, int **capacities, int **flows) {
   FILE *f = fopen(filename, "r");
@@ -17,75 +17,63 @@ void net_from_dimacs(PNEANet G, char *filename, int **capacities, int **flows) {
       sscanf(buf, "p max %d %d", &max_nodes, &max_edges);
       *capacities = (int *) calloc(2*max_edges, sizeof(int));
       *flows = (int *) calloc(2*max_edges, sizeof(int));
-    }
-    if (buf[0] == 'a') {
+    } else if (buf[0] == 'a') {
       sscanf(buf, "%c %d %d %d", &z, &a, &b, &c);
       if (!G->IsNode(a-1)) { G->AddNode(a-1); }
       if (!G->IsNode(b-1)) { G->AddNode(b-1); }
       if (!G->IsEdge(a-1,b-1)) { G->AddEdge(a-1,b-1); }
-      //if (!G->IsEdge(b-1,a-1)) { G->AddEdge(b-1,a-1); }
+      if (!G->IsEdge(b-1,a-1)) { G->AddEdge(b-1,a-1); }
       e_id = G->GetEId(a-1,b-1);
-      G->AddIntAttrDatE(e_id, c, "capacity");
       (*capacities)[e_id] = c;
-      //e_id = G->GetEId(b-1,a-1);
-      //G->AddIntAttrDatE(e_id, 0, "capacity");
-    }
-  }
-  // Make Undirected
-  for (TNEANet::TEdgeI EI = G->BegEI(); EI < G->EndEI(); EI++) {
-    int s_id = EI.GetSrcNId();
-    int d_id = EI.GetDstNId();
-    if (! G->IsEdge(d_id, s_id)) {
-      int e_id = G->AddEdge(d_id, s_id);
-      G->AddIntAttrDatE(e_id, 0, "capacity");
     }
   }
   fclose(f);
 }
 
 
-void init_flow(PNEANet G) {
-  for (TNEANet::TEdgeI EI = G->BegEI(); EI < G->EndEI(); EI++) {
-    G->AddIntAttrDatE(EI.GetId(), 0, "flow");
-  }
-}
-
-
-static inline void push(PNEANet G, int u, int v, int *e) {
+static inline void push(PNEANet G, int u, int v, int *e, int *capacities, int *flows) {
   int e_id = G->GetEId(u,v);
   int rev_e_id = G->GetEId(v,u);
-  int f = G->GetIntAttrDatE(e_id, "flow");
+  int f = flows[e_id];
 
-  int delta = G->GetIntAttrDatE(e_id, "capacity") - f;
+  int delta = capacities[e_id] - f;
   int amt = MIN( e[u], delta );
-  G->AddIntAttrDatE(e_id, f + amt, "flow");
-  G->AddIntAttrDatE(rev_e_id, -(f + amt), "flow");
+  flows[e_id] = f + amt;
+  flows[rev_e_id] = -(f + amt);
   e[u] -= amt;
   e[v] += amt;
 }
 
 
-static inline void global_relabel(PNEANet G, int t, int *h) {
+static inline void global_relabel(PNEANet G, int t, int *h, int *capacities, int *flows) {
   //TODO: Change this to avoid the fact that we are using a 1 indexed node
   //TODO: can be done without linear pass, also use a constant buffer
+  //static TSnapQueue<int> node_queue(G->GetNodes());
+  static int *bfs_queue = (int *) calloc(G->GetNodes(), sizeof(int));
   for (int i = 0; i < G->GetNodes(); ++i) {
     h[i] = 10000000;
   }
   h[t] = 0;
   TNEANet::TNodeI NI = G->GetNI(t);
-  TSnapQueue<int> node_queue(G->GetNodes());
-  node_queue.Push(t);
-  while (node_queue.Empty() == 0) {
-    int cur_node = node_queue.Top();
-    node_queue.Pop();
+  //TSnapQueue<int> node_queue(G->GetNodes());
+  int left = 0;
+  int right = 1;
+  //node_queue.Push(t);
+  bfs_queue[0] = t;
+  while (left != right) {
+    int cur_node = bfs_queue[left];//node_queue.Top();
+    left++;
+    //node_queue.Pop();
     NI = G->GetNI(cur_node);
     for (int i = 0; i < NI.GetInDeg(); ++i) {
       int prev_node = NI.GetInNId(i);
       if (h[prev_node] > h[cur_node] + 1) {
         int e_id = G->GetEId(prev_node, cur_node);
-        if (G->GetIntAttrDatE(e_id, "capacity") - G->GetIntAttrDatE(e_id, "flow") > 0) {
+        if (capacities[e_id] - flows[e_id] > 0) {
           h[prev_node] = h[cur_node] + 1;
-          node_queue.Push(prev_node);
+          //node_queue.Push(prev_node);
+          bfs_queue[right] = prev_node;
+          right++;
         }
       }
     }
@@ -93,19 +81,19 @@ static inline void global_relabel(PNEANet G, int t, int *h) {
 }
 
 
-static inline void relabel(PNEANet G, int u, int t, int *h) {
+static inline void relabel(PNEANet G, int u, int t, int *h, int *capacities, int *flows) {
   static int counter = 0;
   counter++;
   if (counter > GLOBAL_UPDATE_FREQ*G->GetNodes()) {
     counter = 0;
-    global_relabel(G, t, h);
+    global_relabel(G, t, h, capacities, flows);
   }
   TNEANet::TNodeI NI = G->GetNI(u);
   int min_neighbor = INF;
   for (int i = 0; i < NI.GetOutDeg(); ++i) {
     int v = NI.GetOutNId(i);
     int e_id = G->GetEId(u,v);
-    if (G->GetIntAttrDatE(e_id, "capacity") - G->GetIntAttrDatE(e_id, "flow") > 0 ) {
+    if (capacities[e_id] - flows[e_id] > 0) {
       if (min_neighbor > h[v]) { min_neighbor = h[v]; }
     }
   }
@@ -115,19 +103,19 @@ static inline void relabel(PNEANet G, int u, int t, int *h) {
 
 int push_relabel(PNEANet G, int s, int t, int *capacities, int *flows) {
   // Init
-  init_flow(G);
+  //init_flow(G);
   int min_height, u, v, n = G->GetNodes();
   int *height = (int *) calloc(n+1, sizeof(int));
   int *excess = (int *) calloc(n+1, sizeof(int));
   bool *in_queue = (bool *) calloc(n+1, sizeof(bool));
-  global_relabel(G, t, height);
-  //height[s] = n;
+  global_relabel(G, t, height, capacities, flows);
+  height[s] = n;
   excess[s] = INF;
   TNEANet::TNodeI NI = G->GetNI(s);
   TSnapQueue<int> node_queue(n);
   for (int i = 0; i < NI.GetOutDeg(); ++i) {
     v = NI.GetOutNId(i);
-    push(G, s, v, excess);
+    push(G, s, v, excess, capacities, flows);
     node_queue.Push(v);
     in_queue[v] = 1;
   }
@@ -139,9 +127,9 @@ int push_relabel(PNEANet G, int s, int t, int *capacities, int *flows) {
     for (int i = 0; i < NI.GetOutDeg(); ++i) {
       v = NI.GetOutNId(i);
       int e_id = G->GetEId(u,v);
-      if (G->GetIntAttrDatE(e_id, "capacity") - G->GetIntAttrDatE(e_id, "flow") > 0 ) {
+      if (capacities[e_id] - flows[e_id] > 0) {
         if (height[u] > height[v]) {
-          push(G, u, v, excess);
+          push(G, u, v, excess, capacities, flows);
           if (in_queue[v] == 0 && v != s && v != t) {
             in_queue[v] = 1;
             node_queue.Push(v);
@@ -154,7 +142,7 @@ int push_relabel(PNEANet G, int s, int t, int *capacities, int *flows) {
       in_queue[u] = 0;
       node_queue.Pop();
     } else if (excess[u] > 0) {
-      relabel(G, u, t, height);
+      relabel(G, u, t, height, capacities, flows);
     }
   }
 
---
+++ examples/flow/run_all_tests.sh
@@ -0,0 +1,13 @@
+make all
+
+./pushrelabel Tests/rmflong_8_64.txt
+./pushrelabel Tests/rmflong_10_91.txt
+./pushrelabel Tests/rmflong_13_181.txt
+./pushrelabel Tests/rmflong_16_256.txt
+./pushrelabel Tests/rmflong_19_362.txt
+#./pushrelabel Tests/rmflong_23_512.txt
+
+./pushrelabel Tests/rmfwide_28_5.txt
+./pushrelabel Tests/rmfwide_37_6.txt
+./pushrelabel Tests/rmfwide_49_7.txt
+#./pushrelabel Tests/rmfwide_64_8.txt
