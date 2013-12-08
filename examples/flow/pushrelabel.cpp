8f83139e8fac6c30256177a8913da9a114d2f13f
Omede <omedefirouz@gmail.com>
2013-12-08 14:43:24 -0800

    Highest Label, but broken

--- examples/flow/pushrelabel.cpp
+++ examples/flow/pushrelabel.cpp
@@ -13,6 +13,7 @@ const static int beta = 12;
 static int NUM_NODES = 0;
 static int NUM_EDGES = 0;
 
+static int max_distance = 0;
 static int max_active = 0;
 static int min_active = 0;
 
@@ -44,8 +45,8 @@ typedef struct layer {
 void add_to_active_layer(Layer *layers, int layer, Node *nodes, int i) {
   nodes[i].next_active = layers[layer].first_active;
   layers[layer].first_active = i;
-  if (min_active < nodes[i].height) { min_active = nodes[i].height; }
-  if (max_active > nodes[i].height) { max_active = nodes[i].height; }
+  if (min_active > nodes[i].height) { min_active = nodes[i].height; }
+  if (max_active < nodes[i].height) { max_active = nodes[i].height; }
 }
 
 
@@ -63,7 +64,7 @@ void add_to_inactive_layer(Layer *layers, int layer, Node *nodes, int i) {
 }
 
 
-void remove_From_inactive_layer(Layer *layers, int layer, Node *nodes, int i) {
+void remove_from_inactive_layer(Layer *layers, int layer, Node *nodes, int i) {
   if (layers[layer].first_active == i) {
     layers[layer].first_inactive = nodes[i].next_inactive;
     nodes[nodes[i].next_inactive].prev_inactive = -layer;
@@ -119,8 +120,7 @@ static inline void push(PNEANet G, int u, int v, Node *nodes, Arc *arcs) {
 }
 
 
-//static inline void global_relabel(PNEANet G, int t, Node *nodes, int *capacities, int *flows) {
-static inline void global_relabel(PNEANet G, int t, Node *nodes, Arc *arcs) {
+static inline void global_relabel(PNEANet G, int t, Node *nodes, Arc *arcs, Layer *layers) {
   //TODO: globals are bad!
   static int *bfs_queue = (int *) malloc(G->GetNodes()*sizeof(int));
   for (int i = 0; i < NUM_NODES; ++i) {
@@ -128,22 +128,42 @@ static inline void global_relabel(PNEANet G, int t, Node *nodes, Arc *arcs) {
   }
   nodes[t].height = 0;
   TNEANet::TNodeI NI = G->GetNI(t);
+
+
+  //TODO: doesn't need to do all layers, can be more efficient later after gaps
+  for (int i = 0; i <= max_distance; ++i) {
+    layers[i].first_active = -1;
+    layers[i].first_inactive = -1;
+  }
+
+  max_distance = max_active = 0;
+  min_active = NUM_NODES;
+
+
   int left = 0;
   int right = 1;
   bfs_queue[0] = t;
   while (left != right) {
-    int cur_node = bfs_queue[left];
-    left++;
+    int cur_node = bfs_queue[left++];
     NI = G->GetNI(cur_node);
     for (int i = 0; i < NI.GetInDeg(); ++i) {
       int prev_node = NI.GetInNId(i);
       if (nodes[prev_node].height > nodes[cur_node].height + 1) {
         int e_id = G->GetEId(prev_node, cur_node);
-        //if (capacities[e_id] - flows[e_id] > 0) {
         if (arcs[e_id].capacity - arcs[e_id].flow > 0) {
           nodes[prev_node].height = nodes[cur_node].height + 1;
-          bfs_queue[right] = prev_node;
-          right++;
+
+          if (max_distance < nodes[prev_node].height) {
+            max_distance = nodes[prev_node].height;
+          }
+
+          if (nodes[prev_node].excess > 0) {
+            add_to_active_layer(layers, nodes[prev_node].height, nodes, prev_node);
+          } else {
+            add_to_inactive_layer(layers, nodes[prev_node].height, nodes, prev_node);
+          }
+
+          bfs_queue[right++] = prev_node;
         }
       }
     }
@@ -151,13 +171,26 @@ static inline void global_relabel(PNEANet G, int t, Node *nodes, Arc *arcs) {
 }
 
 
-//static inline void relabel(PNEANet G, int u, int t, Node *nodes, int *capacities, int *flows) {
-static inline void relabel(PNEANet G, int u, int t, Node *nodes, Arc *arcs) {
+static inline void gap_relabel(Layer *layers, Node *nodes, int empty_distance) {
+  int cutoff = empty_distance - 1;
+  for (int i = empty_distance + 1; i < max_distance; ++i) {
+    int node_index = layers[i].first_inactive;
+    while (node_index >= 0) {
+      nodes[node_index].height = NUM_NODES;
+      node_index = nodes[node_index].next_inactive;
+    }
+    layers[i].first_inactive = -1;
+  }
+  max_distance = max_active = cutoff;
+}
+
+
+static inline void relabel(PNEANet G, int u, int t, Node *nodes, Arc *arcs, Layer *layers) {
   static int counter = 0;
   counter++;
   if (counter > global_update_freq*NUM_NODES) {
     counter = 0;
-    global_relabel(G, t, nodes, arcs);
+    global_relabel(G, t, nodes, arcs, layers);
   }
   /*TNEANet::TNodeI NI = G->GetNI(u);
   int min_neighbor = INF;
@@ -171,6 +204,8 @@ static inline void relabel(PNEANet G, int u, int t, Node *nodes, Arc *arcs) {
   h[u] = min_neighbor + 1;
   */
 }
+
+void discharge(PNEANet G, Layer *layers, Node *nodes, Arc *arcs) { return; }
 
 
 int push_relabel(PNEANet G, int s, int t, Arc *arcs) {
@@ -188,52 +223,84 @@ int push_relabel(PNEANet G, int s, int t, Arc *arcs) {
     nodes[i].next_active = -1;
   }
 
-  bool *in_queue = (bool *) calloc(NUM_NODES, sizeof(bool)); //TODO: Deprecated
+  //bool *in_queue = (bool *) calloc(NUM_NODES, sizeof(bool)); //TODO: Deprecated
 
   int min_neighbor;
   min_active = NUM_NODES;
   max_active = 0;
 
-  global_relabel(G, t, nodes, arcs); // CANNOT DO AFTER HEIGHT/EXCESS
+  //global_relabel(G, t, nodes, arcs, layers); // CANNOT DO AFTER HEIGHT/EXCESS
   nodes[s].height = NUM_NODES;
   nodes[s].excess = INF;
 
   TNEANet::TNodeI NI = G->GetNI(s);
-  TSnapQueue<int> node_queue(NUM_NODES);
+  //TSnapQueue<int> node_queue(NUM_NODES);
   for (int i = 0; i < NI.GetOutDeg(); ++i) {
     v = NI.GetOutNId(i);
     push(G, s, v, nodes, arcs);
-    node_queue.Push(v);
-    in_queue[v] = 1;
+    //node_queue.Push(v);
+    //in_queue[v] = 1;
   }
+  max_active = 0;
+  min_active = NUM_NODES;
+  max_distance = NUM_NODES - 1;
 
-  //while (max_active >= min_active) {
-
-  while (node_queue.Empty() == 0) {
-    min_neighbor = INF;
-    u = node_queue.Top();
-    NI = G->GetNI(u);
-    for (int i = 0; i < NI.GetOutDeg(); ++i) {
-      v = NI.GetOutNId(i);
-      int e_id = G->GetEId(u,v);
-      if (arcs[e_id].capacity - arcs[e_id].flow > 0) {
-        if (nodes[u].height > nodes[v].height) {
-          push(G, u, v, nodes, arcs);
-          if (in_queue[v] == 0 && v != s && v != t) {
-            in_queue[v] = 1;
-            node_queue.Push(v);
-          }
-        }
-        if (min_neighbor > nodes[v].height) { min_neighbor = nodes[v].height; }
+  for (int i = 0; i < NUM_NODES; ++i) {
+    if (i != s && i != t) {
+      if (nodes[i].excess > 0) {
+        add_to_active_layer(layers, nodes[i].height, nodes, i);
+      } else if (nodes[i].height < NUM_NODES) {
+        add_to_inactive_layer(layers, nodes[i].height, nodes, i);
       }
     }
+  }
+  printf("\n%d %d\n", max_active, min_active);
 
-    if (nodes[u].excess == 0) {
-      in_queue[u] = 0;
-      node_queue.Pop();
-    } else if (nodes[u].excess > 0) {
-      nodes[u].height = min_neighbor + 1;
-      relabel(G, u, t, nodes, arcs);
+  while (max_active >= min_active) {
+  printf("\n%d %d\n", max_active, min_active);
+    if (layers[max_active].first_active < 0) {
+      max_active--;
+    } else {
+      u = layers[max_active].first_active;
+      remove_from_active_layer(layers, nodes[u].height, nodes, u);
+      discharge(G, layers, nodes, arcs);
+
+      /*
+    //while (node_queue.Empty() == 0) {
+      min_neighbor = INF;
+      //u = node_queue.Top();
+      NI = G->GetNI(u);
+      for (int i = 0; i < NI.GetOutDeg(); ++i) {
+        v = NI.GetOutNId(i);
+        int e_id = G->GetEId(u,v);
+        if (arcs[e_id].capacity - arcs[e_id].flow > 0) {
+          if (nodes[u].height > nodes[v].height) {
+            push(G, u, v, nodes, arcs);
+            if (v != s && v != t && nodes[v].excess == 0) {
+              remove_from_inactive_layer(layers, nodes[v].height, nodes, v);
+              add_to_active_layer(layers, nodes[v].height, nodes, v);
+            //if (in_queue[v] == 0 && v != s && v != t) {
+              //in_queue[v] = 1;
+              //node_queue.Push(v);
+
+
+            }
+          }
+          if (min_neighbor > nodes[v].height) { min_neighbor = nodes[v].height; }
+        }
+      }
+
+      if (nodes[u].excess == 0) {
+        //in_queue[u] = 0;
+        //node_queue.Pop();
+      } else if (nodes[u].excess > 0) {
+        int old_height = nodes[u].height;
+        nodes[u].height = min_neighbor + 1;
+        if (layers[old_height].first_inactive < 0 && layers[old_height].first_active < 0) {
+          gap_relabel(layers, nodes, old_height);
+        }
+        //relabel(G, u, t, nodes, arcs, layers);
+      }*/
     }
   }
 
@@ -253,6 +320,7 @@ int main(int argc, char* argv[]) {
 
   //clock_t start_init = clock();
   net_from_dimacs(G, filename, &arcs);
+  printf("NODES: %d EDGES: %d\n", NUM_NODES, NUM_EDGES);
   //clock_t end_init = clock();
 
   //printf("%s\t", filename);
