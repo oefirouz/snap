adcfa3f770b0591edf81787f3136270dd3f7e383
Omede <omedefirouz@gmail.com>
2013-12-08 13:47:05 -0800

    Refactored to use arc structs

--- examples/flow/pushrelabel.cpp
+++ examples/flow/pushrelabel.cpp
@@ -13,13 +13,26 @@ const static int beta = 12;
 static int NUM_NODES = 0;
 static int NUM_EDGES = 0;
 
+static int max_active = 0;
+static int min_active = 0;
+
 typedef struct node {
   int excess;
   int height;
   int next_active;
   int next_inactive;
   int prev_inactive;
+  // int firstarc
 } Node;
+
+
+typedef struct arc {
+  int capacity;
+  int flow;
+  //int available;
+  //int firstNode
+  //int secondNode
+} Arc;
 
 
 typedef struct layer {
@@ -31,6 +44,8 @@ typedef struct layer {
 void add_to_active_layer(Layer *layers, int layer, Node *nodes, int i) {
   nodes[i].next_active = layers[layer].first_active;
   layers[layer].first_active = i;
+  if (min_active < nodes[i].height) { min_active = nodes[i].height; }
+  if (max_active > nodes[i].height) { max_active = nodes[i].height; }
 }
 
 
@@ -59,24 +74,17 @@ void remove_From_inactive_layer(Layer *layers, int layer, Node *nodes, int i) {
 }
 
 
-/*typedef struct arc {
-  int capacity;
-  int flow;
-  //int available;
-} Arc;*/
-
-
-void net_from_dimacs(PNEANet G, char *filename, int **capacities, int **flows) {
+void net_from_dimacs(PNEANet G, char *filename, Arc **arcs) {
   FILE *f = fopen(filename, "r");
   if (f == NULL) { printf("Couldn't find file!\n"); return; }
   int a, b, c, e_id, max_edges, max_nodes;
   char z, buf[1024];
+  int *capacities;
   while (fgets(buf, 1024, f) != NULL) {
     if (buf[0] == 'p') {
       sscanf(buf, "p max %d %d", &max_nodes, &max_edges);
       NUM_NODES = max_nodes;
-      *capacities = (int *) calloc(2*max_edges, sizeof(int));
-      *flows = (int *) calloc(2*max_edges, sizeof(int));
+      capacities = (int *) calloc(2*max_edges, sizeof(int));
     } else if (buf[0] == 'a') {
       sscanf(buf, "%c %d %d %d", &z, &a, &b, &c);
       if (!G->IsNode(a-1)) { G->AddNode(a-1); }
@@ -84,30 +92,36 @@ void net_from_dimacs(PNEANet G, char *filename, int **capacities, int **flows) {
       if (!G->IsEdge(a-1,b-1)) { G->AddEdge(a-1,b-1); NUM_EDGES++; }
       if (!G->IsEdge(b-1,a-1)) { G->AddEdge(b-1,a-1); NUM_EDGES++; }
       e_id = G->GetEId(a-1,b-1);
-      (*capacities)[e_id] = c;
+      capacities[e_id] = c;
     }
   }
+  *arcs = (Arc *) calloc(NUM_EDGES, sizeof(Arc));
+  for (int i = 0; i < NUM_EDGES; ++i) {
+    (*arcs)[i].capacity = capacities[i];
+  }
+  free(capacities);
   fclose(f);
 }
 
 
-static inline void push(PNEANet G, int u, int v, Node *nodes, int *capacities, int *flows) {
+static inline void push(PNEANet G, int u, int v, Node *nodes, Arc *arcs) {
   int e_id = G->GetEId(u,v);
   int rev_e_id = G->GetEId(v,u);
-  int f = flows[e_id];
+  int f = arcs[e_id].flow;
 
-  int delta = capacities[e_id] - f;
+  int delta = arcs[e_id].capacity - f;
   int ex = nodes[u].excess;
   int amt = MIN( ex, delta );
-  flows[e_id] = f + amt;
-  flows[rev_e_id] = -(f + amt);
+  arcs[e_id].flow = f + amt;
+  arcs[rev_e_id].flow = -(f + amt);
   nodes[u].excess -= amt;
   nodes[v].excess += amt;
 }
 
 
-static inline void global_relabel(PNEANet G, int t, Node *nodes, int *capacities, int *flows) {
-  //TODO: can be done without linear pass, also use a constant buffer
+//static inline void global_relabel(PNEANet G, int t, Node *nodes, int *capacities, int *flows) {
+static inline void global_relabel(PNEANet G, int t, Node *nodes, Arc *arcs) {
+  //TODO: globals are bad!
   static int *bfs_queue = (int *) malloc(G->GetNodes()*sizeof(int));
   for (int i = 0; i < NUM_NODES; ++i) {
     nodes[i].height = NUM_NODES; //INF;
@@ -125,7 +139,8 @@ static inline void global_relabel(PNEANet G, int t, Node *nodes, int *capacities
       int prev_node = NI.GetInNId(i);
       if (nodes[prev_node].height > nodes[cur_node].height + 1) {
         int e_id = G->GetEId(prev_node, cur_node);
-        if (capacities[e_id] - flows[e_id] > 0) {
+        //if (capacities[e_id] - flows[e_id] > 0) {
+        if (arcs[e_id].capacity - arcs[e_id].flow > 0) {
           nodes[prev_node].height = nodes[cur_node].height + 1;
           bfs_queue[right] = prev_node;
           right++;
@@ -136,12 +151,13 @@ static inline void global_relabel(PNEANet G, int t, Node *nodes, int *capacities
 }
 
 
-static inline void relabel(PNEANet G, int u, int t, Node *nodes, int *capacities, int *flows) {
+//static inline void relabel(PNEANet G, int u, int t, Node *nodes, int *capacities, int *flows) {
+static inline void relabel(PNEANet G, int u, int t, Node *nodes, Arc *arcs) {
   static int counter = 0;
   counter++;
   if (counter > global_update_freq*NUM_NODES) {
     counter = 0;
-    global_relabel(G, t, nodes, capacities, flows);
+    global_relabel(G, t, nodes, arcs);
   }
   /*TNEANet::TNodeI NI = G->GetNI(u);
   int min_neighbor = INF;
@@ -157,15 +173,28 @@ static inline void relabel(PNEANet G, int u, int t, Node *nodes, int *capacities
 }
 
 
-int push_relabel(PNEANet G, int s, int t, int *capacities, int *flows) {
+int push_relabel(PNEANet G, int s, int t, Arc *arcs) {
   int u, v;
 
   Node *nodes = (Node *) calloc(NUM_NODES, sizeof(Node));
+  Layer *layers = (Layer *) calloc(NUM_NODES + 1, sizeof(Layer));
+  for (int i = 0; i <= NUM_NODES; ++i) {
+    layers[i].first_active = -1;
+    layers[i].first_inactive = -1;
+  }
+  for (int i = 0; i < NUM_NODES; ++i) {
+    nodes[i].prev_inactive = -1;
+    nodes[i].next_inactive = -1;
+    nodes[i].next_active = -1;
+  }
+
   bool *in_queue = (bool *) calloc(NUM_NODES, sizeof(bool)); //TODO: Deprecated
 
   int min_neighbor;
+  min_active = NUM_NODES;
+  max_active = 0;
 
-  global_relabel(G, t, nodes, capacities, flows); // CANNOT DO AFTER HEIGHT/EXCESS
+  global_relabel(G, t, nodes, arcs); // CANNOT DO AFTER HEIGHT/EXCESS
   nodes[s].height = NUM_NODES;
   nodes[s].excess = INF;
 
@@ -173,10 +202,12 @@ int push_relabel(PNEANet G, int s, int t, int *capacities, int *flows) {
   TSnapQueue<int> node_queue(NUM_NODES);
   for (int i = 0; i < NI.GetOutDeg(); ++i) {
     v = NI.GetOutNId(i);
-    push(G, s, v, nodes, capacities, flows);
+    push(G, s, v, nodes, arcs);
     node_queue.Push(v);
     in_queue[v] = 1;
   }
+
+  //while (max_active >= min_active) {
 
   while (node_queue.Empty() == 0) {
     min_neighbor = INF;
@@ -185,9 +216,9 @@ int push_relabel(PNEANet G, int s, int t, int *capacities, int *flows) {
     for (int i = 0; i < NI.GetOutDeg(); ++i) {
       v = NI.GetOutNId(i);
       int e_id = G->GetEId(u,v);
-      if (capacities[e_id] - flows[e_id] > 0) {
+      if (arcs[e_id].capacity - arcs[e_id].flow > 0) {
         if (nodes[u].height > nodes[v].height) {
-          push(G, u, v, nodes, capacities, flows);
+          push(G, u, v, nodes, arcs);
           if (in_queue[v] == 0 && v != s && v != t) {
             in_queue[v] = 1;
             node_queue.Push(v);
@@ -202,7 +233,7 @@ int push_relabel(PNEANet G, int s, int t, int *capacities, int *flows) {
       node_queue.Pop();
     } else if (nodes[u].excess > 0) {
       nodes[u].height = min_neighbor + 1;
-      relabel(G, u, t, nodes, capacities, flows);
+      relabel(G, u, t, nodes, arcs);
     }
   }
 
@@ -218,17 +249,17 @@ int main(int argc, char* argv[]) {
   char *filename = argv[1];
   PNEANet G = PNEANet::New();
 
-  int *capacities, *flows;
+  Arc *arcs;
 
   //clock_t start_init = clock();
-  net_from_dimacs(G, filename, &capacities, &flows);
+  net_from_dimacs(G, filename, &arcs);
   //clock_t end_init = clock();
 
   //printf("%s\t", filename);
   //printf("Init:%f\t", ((float)end_init - start_init)/CLOCKS_PER_SEC);
   //fflush(stdout);
   clock_t start = clock();
-  int flow = push_relabel(G, 0, G->GetNodes()-1, capacities, flows);
+  int flow = push_relabel(G, 0, G->GetNodes()-1, arcs);
   clock_t end = clock();
   printf("%d\t%d\t", NUM_NODES, NUM_EDGES);
   printf("%d\t", flow);
--- examples/flow/run_all_tests.sh
+++ examples/flow/run_all_tests.sh
@@ -2,11 +2,14 @@ echo "Making SNAP Push-Relabel"
 make all
 
 echo "Making BOOST Push-Relabel"
-g++ -O3 -I /usr/local/Cellar/boost/1.54.0/ boost_flow.cpp -o boost_flow
+#g++ -O3 -I /usr/local/Cellar/boost/1.54.0/ boost_flow.cpp -o boost_flow
 
 
 for filename in "rmflong_8_64" "rmflong_10_91" "rmflong_11_128" "rmflong_13_181" \
-"rmflong_19_362"
+"rmflong_16_256" "rmflong_19_362" "rmflong_23_512" \
+"rmfwide_28_5" "rmfwide_37_6" "rmfwide_49_7" "rmfwide_64_8" #"rmfwide_85_9"
+#"rmfwide_111_10" "rmfwide_147_12" "rmfwide_194_14"
+#"rmflong_16_256" "rmflong_19_362" "rmflong_23_512" "rmflong_30_724" \
 do
     printf $filename
     printf "\t"
